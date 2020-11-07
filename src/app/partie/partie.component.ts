fdb27fe0d22ce554a72d02027087f4605589f931
BONGIORNO <bongiorno.leo@gmail.com>
2020-11-07 19:32:17 +0100

    fix issue with socket io

--- src/app/partie/partie.component.ts
+++ src/app/partie/partie.component.ts
@@ -4,45 +4,52 @@ import { partieModel } from '../datamodel/partieModel';
 import { mancheModel } from '../datamodel/mancheModel';
 import { MancheService } from '../services/manche.service';
 
-import io from "socket.io-client";
-import { back } from "../utils/urls";
+import { io } from 'socket.io-client';
+import { back } from '../utils/urls';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-partie',
   templateUrl: './partie.component.html',
-  styleUrls: ['./partie.component.scss']
+  styleUrls: ['./partie.component.scss'],
 })
 export class PartieComponent implements OnInit {
-
   partie: partieModel = {
-    manches: []
-  }
+    manches: [],
+  };
   private socket: any;
 
   constructor(
     private mancheService: MancheService,
     private userService: UserService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getManches();
     this.socket = io(`${back}`);
   }
-  AfterViewInit(): void {
-    this.socket.on('question')
-  }
-
-  getManches(): void {
-    this.mancheService.getManches().toPromise().then((res: mancheModel[]) => {
-      this.partie.manches = this.mancheService.fromBackToManche(res);
-    }, error => {
-      console.log(error);
+  ngAfterViewInit(): void {
+    this.socket.on('question', (data) => {
+      console.log(data);
     });
   }
 
+  getManches(): void {
+    this.mancheService
+      .getManches()
+      .toPromise()
+      .then(
+        (res: mancheModel[]) => {
+          this.partie.manches = this.mancheService.fromBackToManche(res);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
   onClick() {
-    this.socket.emit("next", this.userService.getCurrentUser().pseudo);
+    this.socket.emit('next', this.userService.getCurrentUser().pseudo);
     alert(this.userService.getCurrentUser().pseudo);
   }
 }
--- src/app/utils/urls.ts
+++ src/app/utils/urls.ts
@@ -1,4 +1,5 @@
 export const back = 'https://lockdown-server.herokuapp.com/';
+//export const back = 'http://localhost:3000/';
 export const suggestions = `${back}suggestions`;
 
 
