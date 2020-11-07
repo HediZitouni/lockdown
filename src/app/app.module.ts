9c7fbd342900554ec53f5f0e90318bbf4c09ceb1
HediZitouni <zitouni.hedi1@gmail.com>
2020-11-07 17:56:04 +0100

    remove duplicate http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
